6bf992ab56595212f1fa2a61b7f17c9c21409ba9
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2015-11-07 18:57:27 -0800

    rails-api new project

---
+++ webservice/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Webservice::Application.config.secret_token = '5439707f9fdb410bb5d902e421d2be4c003f10407986dc82c84118f14db807f4830e9427d469657e0671be92ddc3ff8d673f61f94bef26407027f1f929fa4f31'
